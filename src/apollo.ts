e387feffc06feb5590afb8acd1df72207e557630
Kim Jun Sik <jinaganda5@gmail.com>
2019-09-25 21:45:43 +0900

    Setting clientState for LogIn/Out

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,44 @@
 import ApolloClient, { Operation } from "apollo-boost";
 
 const client = new ApolloClient({
+  clientState: {
+    defaults: {
+      // the one below is cache
+      auth: {
+        _typename: "Auth",
+        isLoggedIn: Boolean(localStorage.getItem("jwt"))
+      }
+    },
+    resolvers: {
+      Mutation: {
+        // logUserIn(parent, context, {cache})
+        logUserIn: (_, { token }, { cache }) => {
+          localStorage.setItem("jwt", token);
+          cache.writeData({
+            data: {
+              auth: {
+                __typename: "Auth",
+                isLoggedIn: true
+              }
+            }
+          });
+          return null;
+        },
+        logUserOut: (_, __, { cache }) => {
+          localStorage.removeItem("jwt");
+          cache.writeData({
+            data: {
+              auth: {
+                __typename: "Auth",
+                isLoggedIn: false
+              }
+            }
+          });
+          return null;
+        }
+      }
+    }
+  },
   // intercept request here
   // 1. call localstorage -> 2. get "jwt" here (redux)
   // for use data in localstorage

68553403394b79b1889ffea543fe9ba334e103c1
Kim Jun Sik <jinaganda5@gmail.com>
2019-09-29 17:17:00 +0900

    set up for GlobalStyle

---
+++ src/global-styles.ts
@@ -0,0 +1,25 @@
+import reset from "styled-reset";
+import { createGlobalStyle } from "./typed-components";
+
+// tslint:disable-next-line
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Maven+Pro');
+    ${reset}
+    * {
+        box-sizing:border-box;
+    }
+    body{
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    }
+    a{
+        color:inherit;
+        text-decoration:none;
+    }
+    input,
+    button{
+        &:focus,
+        &:active{outline:none}
+    }
+`;
+
+export default GlobalStyle;
